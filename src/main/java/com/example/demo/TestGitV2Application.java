af60fac0dc56198f9643afc50a5814c6a30f4e44
rakus-NguyenSon <nhson@rakus.co.jp>
2021-06-04 11:44:26 +0900

    first commit

---
+++ src/main/java/com/example/demo/TestGitV2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitV2Application.class, args);
+	}
+
+}
